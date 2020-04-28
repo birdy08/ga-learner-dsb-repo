0f08ca9d6d4b387cd94db3b6ba2b5d2977a13b7a
Birdy-18 <Birdy-18@users.noreply.github.com>
2020-04-28 09:01:44 +0530

    Code file created.

---
+++ Manipulating-data-using-numpy-/code.py
@@ -0,0 +1,85 @@
+# --------------
+import numpy as np
+
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+data = np.genfromtxt(path, delimiter=",", skip_header=1)
+print(data.shape)
+census=np.concatenate((data, new_record),axis = 0)
+print(census.shape)
+
+
+
+# --------------
+#Code starts here
+import numpy as np 
+age=census[:,0]
+print(age)
+max_age = np.max(age)
+print(max_age)
+min_age = np.min(age)
+print(min_age)
+age_mean = np.mean(age)
+print(age_mean)
+age_std = np.std(age)
+print(age_std)
+
+
+# --------------
+#Code starts here
+import numpy as np
+
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+
+len_0=len(race_0)
+len_1=len(race_1)
+len_2=len(race_2)
+len_3=len(race_3)
+len_4=len(race_4)
+
+print('Race_0: ', len_0)
+print('Race_1: ', len_1)
+print('Race_2: ', len_2)
+print('Race_3: ', len_3)
+print('Race_4: ', len_4)
+
+race_list=[len_0, len_1,len_2, len_3, len_4]
+
+minority_race=race_list.index(min(race_list))
+
+
+
+# --------------
+#Code starts here
+senior_citizens = census[census[:, 0 ]> 60]
+print(senior_citizens)
+
+working_hours_sum =senior_citizens.sum(axis=0)[6]
+print(working_hours_sum)
+
+senior_citizens_len=len(senior_citizens)
+print(senior_citizens_len)
+
+avg_working_hours=working_hours_sum/senior_citizens_len
+
+print((avg_working_hours))
+
+
+
+
+
+# --------------
+#Code starts here
+
+
+high = census[census[:,1] >10] 
+low =  census[census[:,1] <=10] 
+avg_pay_high=high[:,7].mean()
+print(avg_pay_high)
+avg_pay_low=low[:,7].mean()
+print(avg_pay_low)
+
+
